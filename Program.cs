e10590d527f022166ab93d18f7110c3fde356166
ersukhpreet1008 <76466771+ersukhpreet1008@users.noreply.github.com>
2020-12-22 12:59:02 +0530

    All Codes File

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TableMultiplyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, multiply;
+
+            
+            Console.WriteLine("Enter the Table Number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (multiply = 1; multiply <=10; multiply++ )
+            {
+                Console.WriteLine("{0} * {1} = {2}", number, multiply,(number * multiply));
+            }
+
+        }
+    }
+}
